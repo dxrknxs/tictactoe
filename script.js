80541b1de1eb463417a3f3cc1a8b32e2224c0db9
dxrknxs <84136527+dxrknxs@users.noreply.github.com>
2023-10-12 21:01:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// HTML-Elemente
 const btnRef = document.querySelectorAll(".button-option");
 const popupRef = document.querySelector(".popup");
 const newgameBtn = document.getElementById("new-game");
 const restartBtn = document.getElementById("restart");
 const msgRef = document.getElementById("message");
 
-// Gewinnmuster-Array
 const winningPattern = [
   [0, 1, 2],
   [0, 3, 6],
@@ -17,28 +15,22 @@ const winningPattern = [
   [2, 4, 6],
 ];
 
-// Spieler 'X' beginnt
 let xTurn = true;
 let count = 0;
 
-// Alle Buttons deaktivieren
 const disableButtons = () => {
   btnRef.forEach((element) => (element.disabled = true));
-  // Popup aktivieren
   popupRef.classList.remove("hide");
 };
 
-// Alle Buttons aktivieren (Neues Spiel und Neustart)
 const enableButtons = () => {
   btnRef.forEach((element) => {
     element.innerText = "";
     element.disabled = false;
   });
-  // Popup deaktivieren
   popupRef.classList.add("hide");
 };
 
-// Funktion für den Gewinnfall
 const winFunction = (letter) => {
   disableButtons();
   if (letter == "X") {
@@ -48,27 +40,23 @@ const winFunction = (letter) => {
   }
 };
 
-// Funktion für ein Unentschieden
 const drawFunction = () => {
   disableButtons();
   msgRef.innerHTML = "&#x1F60E; <br> Unentschieden";
 };
 
-// Neue Spiel-Funktion
 const newGame = () => {
   count = 0;
   enableButtons();
-  xTurn = true; // Spieler 'X' beginnt
+  xTurn = true; 
 };
 
-// Neustart-Funktion
 const restartGame = () => {
   count = 0;
   enableButtons();
-  xTurn = true; // Spieler 'X' beginnt
+  xTurn = true;
 };
 
-// Funktion, um einen verfügbaren leeren Button-Index zu erhalten
 const getEmptyButtonIndex = () => {
   const emptyIndexes = [];
   for (let i = 0; i < btnRef.length; i++) {
@@ -83,16 +71,12 @@ const playAIWithDelay = () => {
     const emptyIndexes = getEmptyButtonIndex();
   
     if (emptyIndexes.length > 0) {
-      // Zufälligen leeren Button auswählen
       const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
-      // AI-Zug (angenommen, AI spielt "O")
       btnRef[randomIndex].innerText = "O";
       btnRef[randomIndex].disabled = true;
   
-      // Zähler nach AI-Zug inkrementieren
       count += 1;
   
-      // Nach AI-Zug auf Gewinn prüfen
       winChecker();
   
       if (count === 9) {
@@ -101,7 +85,6 @@ const playAIWithDelay = () => {
     }
   };
 
-// Gewinnprüfung
 const winChecker = () => {
   for (let i of winningPattern) {
     let [element1, element2, element3] = [
@@ -118,44 +101,33 @@ const winChecker = () => {
   }
 };
 
-// Event-Listener für die Buttons
 btnRef.forEach((element) => {
   element.addEventListener("click", () => {
     if (xTurn && element.innerText === "") {
-      // 'X' anzeigen
       element.innerText = "X";
       element.disabled = true;
 
-      // Zähler nach Spielerzug inkrementieren
       count += 1;
 
-      // Nach Spielerzug auf Gewinn prüfen
       winChecker();
 
       if (count === 9) {
         drawFunction();
       }
 
-      // AI spielt nach 1 Sekunde Verzögerung nach dem Spielerzug
       playAIWithDelay();
     }
   });
 });
 
-// Spiel initialisieren
 window.onload = () => {
-  // Spiel initialisieren
   newGame();
 };
 
-// Event-Listener für Neues Spiel
 newgameBtn.addEventListener("click", () => {
-  // Neues Spiel starten
   newGame();
 });
 
-// Event-Listener für Neustart
 restartBtn.addEventListener("click", () => {
-  // Spiel neustarten
   restartGame();
 });
